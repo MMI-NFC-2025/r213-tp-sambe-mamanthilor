248e700eb8141ff1921285f13a6563dbe4d3449a
sambe-mamanthilor <maman_thilor.sambe@edu.univ-fcomte.fr>
2026-01-16 15:07:18 +0100

    erreur correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: netlify()
+  adapter: vercel()
 });
